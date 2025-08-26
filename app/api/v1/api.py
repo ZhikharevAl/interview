7248436210f6c497dc2b5175ad52267365ed3559
ZhikharevAl <waltafunk@email.com>
2025-08-26 21:30:13 +0300

    feat: added api

---
+++ app/api/v1/api.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from app.api.v1.endpoints import categories, questions
+
+api_router = APIRouter()
+api_router.include_router(categories.router, prefix="/categories", tags=["categories"])
+api_router.include_router(questions.router, prefix="/questions", tags=["questions"])
