d2dad6c04875cef9cd0f9510cfdb066358a8932b
ZhikharevAl <waltafunk@email.com>
2025-08-27 22:22:24 +0300

    feat: added cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,10 +2,23 @@ from app.api.v1.api import api_router
 from app.core.config import settings
 from app.db.database import Base, engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Interview Prep App", debug=settings.DEBUG)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
