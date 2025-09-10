cedd9b00b6f01acd9224555825e58272cb7b9ae5
ZhikharevAl <waltafunk@email.com>
2025-09-10 23:06:15 +0300

    tests: added conftest for unit tests

---
+++ tests/unit/conftest.py
@@ -0,0 +1,22 @@
+from collections.abc import Generator
+
+import pytest
+from app.db.database import Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
+test_engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+
+
+@pytest.fixture
+def db_session() -> Generator[Session]:
+    """Create a database session for unit testing."""
+    Base.metadata.create_all(bind=test_engine)
+    session = TestingSessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
+        Base.metadata.drop_all(bind=test_engine)
