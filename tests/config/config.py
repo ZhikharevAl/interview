ac40baeef475f5cdcd7c261f0b99043b6b2ceae4
ZhikharevAl <waltafunk@email.com>
2025-09-12 00:46:40 +0300

    chore: updated url config

--- tests/config/config.py
+++ tests/config/config.py
@@ -1,7 +1,7 @@
 class ConfigTests:
     """Configuration settings for the tests."""
 
-    APP_URL = "http://localhost:8000/"
-    CATEGORIES_URL = "/api/v1/categories"
-    QUESTIONS_URL = "/api/v1/questions"
+    APP_URL = "http://localhost:8080/"
+    CATEGORIES_URL = "http://localhost:8080/api/v1/categories/"
+    QUESTIONS_URL = "http://localhost:8080/api/v1/questions/"
     TIMEOUT = 30000
