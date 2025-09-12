1e8c84eeab17969495be38d8cb17fcd7fc4a078c
ZhikharevAl <waltafunk@email.com>
2025-09-12 03:33:29 +0300

    refactor: settings

--- tests/api/clients/categories.py
+++ tests/api/clients/categories.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import APIResponse
 
 from tests.api.clients.base_api import BaseAPI
-from tests.config.config import ConfigTests
+from tests.config.config import settings
 
 
 class CategoriesClient(BaseAPI):
@@ -10,18 +10,18 @@ class CategoriesClient(BaseAPI):
     def create(self, name: str) -> APIResponse:
         """Sends a request to create a new category."""
         payload = {"name": name}
-        return self.http.post(ConfigTests.CATEGORIES_ENDPOINT, json=payload)
+        return self.http.post(settings.CATEGORIES_ENDPOINT, json=payload)
 
     def get_all(self) -> APIResponse:
         """Sends a request to retrieve all categories."""
-        return self.http.get(ConfigTests.CATEGORIES_ENDPOINT)
+        return self.http.get(settings.CATEGORIES_ENDPOINT)
 
     def get_by_id(self, category_id: int) -> APIResponse:
         """Sends a request to retrieve a category by its ID."""
-        path = f"{ConfigTests.CATEGORIES_ENDPOINT}/{category_id}"
+        path = f"{settings.CATEGORIES_ENDPOINT}/{category_id}"
         return self.http.get(path)
 
     def delete(self, category_id: int) -> APIResponse:
         """Sends a request to delete a category by its ID."""
-        path = f"{ConfigTests.CATEGORIES_ENDPOINT}/{category_id}"
+        path = f"{settings.CATEGORIES_ENDPOINT}/{category_id}"
         return self.http.delete(path)
